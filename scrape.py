6bcd63e7db8603f86cbddb6349711ed8e5511386
Isaac <isaac.mauro@gmail.com>
2023-04-27 18:07:51 -0600

    added some variables

--- scrape.py
+++ scrape.py
@@ -2,12 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+linkedin_address = "url here"
+email = "email here"
+password = "password here"
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options= chrome_options)
 driver.get("https://linkedin.com")
-username = driver.find_element(By.ID, "session_key")
-username.send_keys("hahahahahhahahahahha")
-password = driver.find_element(By.ID, 'session_password')
-password.send_keys("hashdfahsdf")
+driver.implicitly_wait(2)
+username_field = driver.find_element(By.ID, 'session_key')
+username_field.send_keys(email)
+password_field = driver.find_element(By.ID, 'session_password')
+password_field.send_keys(password)
+submit = driver.find_element(By.CLASS_NAME, 'sign-in-form__submit-btn--full-width')
+submit.submit()

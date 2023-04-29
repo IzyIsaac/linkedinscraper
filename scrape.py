c200797d988704b4bf962cfeace017a0d4ed841c
Isaac <izyisaac@yoshi.lol>
2023-04-28 20:43:43 -0600

    Working xpath to grab about text

--- scrape.py
+++ scrape.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 load_dotenv()
-profile_url = input("Please paste the linkedin URL you want to scrape: ")
+# profile_url = input("Please paste the linkedin URL you want to scrape: ")
+profile_url = "https://www.linkedin.com/in/roberto-escobar/"
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -19,11 +20,9 @@ password_field = driver.find_element(By.ID, 'session_password')
 password_field.send_keys(environ['LINKEDIN_PASSWORD'])
 submit = driver.find_element(By.CLASS_NAME, 'sign-in-form__submit-btn--full-width')
 submit.submit()
-driver.implicitly_wait(5)
+driver.implicitly_wait(2)
 driver.get(profile_url)
-driver.implicitly_wait(5)
-html = driver.page_source
+driver.implicitly_wait(2)
 
-soup = BeautifulSoup(html, 'html.parser')
-about_div = soup.find('div', {"id": "about"})
-print(about_div)
+about = driver.find_element(By.XPATH, "//div[@id='about']//following-sibling::div[2]//descendant::span[@aria-hidden='true']")
+print(about.text)
